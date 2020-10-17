807805349ef86f161d5705b7b29f15a97682373f
Maikel Arcia Carrazana <macarci@gmail.com>
2020-10-17 18:40:18 -0300

    Add | ENV config for default URI

--- lib/cenit/admin/setup.rb
+++ lib/cenit/admin/setup.rb
@@ -1,16 +1,16 @@
 module Cenit
   module Admin
 
-    DEFAULT_URI = 'http://localhost:3000'
-
     setup do
-      app = self.app
-      config = app.configuration
-      redirect_uris = config.redirect_uris || []
-      unless redirect_uris.include?(DEFAULT_URI)
-        redirect_uris << DEFAULT_URI
-        config.redirect_uris = redirect_uris
-        app.save
+      if (default_uri = ENV["#{self}:default_uri"].presence)
+        app = self.app
+        config = app.configuration
+        redirect_uris = config.redirect_uris || []
+        unless redirect_uris.include?(default_uri)
+          redirect_uris << default_uri
+          config.redirect_uris = redirect_uris
+          app.save
+        end
       end
     end
   end
