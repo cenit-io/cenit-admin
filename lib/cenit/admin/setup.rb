96415b3606cbca9196fd0aed0aeb9f65bf307dde
Maikel Arcia Carrazana <macarci@gmail.com>
2020-11-05 13:31:52 -0300

    Add | Default cloud URL

--- lib/cenit/admin/setup.rb
+++ lib/cenit/admin/setup.rb
@@ -1,8 +1,10 @@
 module Cenit
   module Admin
 
+    DEFAULT_CLOUD_URL = 'http://cenit-admin.s3-website-us-west-2.amazonaws.com'
+
     setup do
-      if (default_uri = ENV["#{self}:default_uri"].presence)
+      if (default_uri = ENV["#{self}:default_uri"].presence || DEFAULT_CLOUD_URL)
         app = self.app
         config = app.configuration
         redirect_uris = config.redirect_uris || []
