e1ebaecbc24a5e803acb7db4c0702bdbc0b21404
Maikel Arcia Carrazana <macarci@gmail.com>
2020-11-26 15:38:44 -0300

    Fix | URIs cenitHost query param

--- lib/cenit/admin/controller.rb
+++ lib/cenit/admin/controller.rb
@@ -5,12 +5,14 @@ module Cenit
 
       get '/' do
         default_oauth_callback_uri = "#{::Cenit.homepage}#{::Cenit.oauth_path}/callback"
-        @uris = uris = redirect_uris - [default_oauth_callback_uri]
-        if uris.size == 1
-          uri =  URI.parse(uris[0])
+        @uris = uris = (redirect_uris - [default_oauth_callback_uri]).map do |uri|
+          uri = URI.parse(uri)
           new_query_ar = URI.decode_www_form(String(uri.query)) << ['cenitHost', Cenit.homepage]
           uri.query = URI.encode_www_form(new_query_ar)
-          redirect_to uri.to_s
+          uri.to_s
+        end
+        if uris.length == 1
+          redirect_to uris[0]
         end
       end
 
@@ -44,7 +46,7 @@ module Cenit
           status = :not_found
         end
         if error
-          render json: { error: error}, status: status
+          render json: { error: error }, status: status
         end
       end
 
