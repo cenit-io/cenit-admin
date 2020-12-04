b152bb6d930dc26797d5a485c26b9b86b0330b54
Maikel Arcia Carrazana <macarci@gmail.com>
2020-12-04 12:48:21 -0300

    Update | Setup

--- lib/cenit/admin/setup.rb
+++ lib/cenit/admin/setup.rb
@@ -4,8 +4,8 @@ module Cenit
     DEFAULT_CLOUD_URL = 'http://cenit-admin.s3-website-us-west-2.amazonaws.com'
 
     setup do
+      app = self.app
       if (default_uri = ENV["#{self}:default_uri"].presence || DEFAULT_CLOUD_URL)
-        app = self.app
         config = app.configuration
         redirect_uris = config.redirect_uris || []
         unless redirect_uris.include?(default_uri)
@@ -14,6 +14,7 @@ module Cenit
           app.save
         end
       end
+      puts("#{self}:redirect_uris", JSON.pretty_generate(app.configuration.redirect_uris))
     end
   end
 end
