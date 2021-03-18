f5415714ed88b323d4904016b2017087b5d6086f
Maikel Arcia Carrazana <macarci@gmail.com>
2021-03-18 16:29:34 -0300

    Update | Using OAuth App module

--- lib/cenit/admin.rb
+++ lib/cenit/admin.rb
@@ -3,13 +3,17 @@ require 'cenit/admin/version'
 module Cenit
   module Admin
     include BuildInApps
+    include OauthApp
 
     app_name 'Cenit Admin'
 
+    oauth_authorization_for 'openid profile email session_access multi_tenant create read update delete digest'
+
+    DEFAULT_CLOUD_URL = 'http://cenit-admin.s3-website-us-west-2.amazonaws.com'
+
+    default_url DEFAULT_CLOUD_URL
   end
 end
 
 require 'cenit/admin/controller'
-require 'cenit/admin/code'
 require 'cenit/admin/config'
-require 'cenit/admin/setup'
--- lib/cenit/admin/controller.rb
+++ lib/cenit/admin/controller.rb
@@ -16,53 +16,6 @@ module Cenit
         end
       end
 
-      get '/authorization/:id' do
-
-        error = status = nil
-        if (auth = Setup::Authorization.where(id: params[:id]).first)
-          token = auth.metadata['redirect_token']
-          if token == params[:redirect_token]
-            if (redirect_uri = auth.metadata['redirect_uri'])
-              if auth.authorized?
-                code = Code.create_from_json(metadata: { auth_id: auth.id.to_s })
-                redirect_uri = "#{redirect_uri}?code=#{URI.encode(code.value)}"
-                if (state = auth.metadata['state'])
-                  redirect_uri = "#{redirect_uri}&state=#{URI.encode(state)}"
-                end
-                redirect_to redirect_uri
-              else
-                redirect_to redirect_uri + '?error=' + URI.encode('Not authorized')
-              end
-            else
-              error = 'Invalid authorization state'
-              status = :not_acceptable
-            end
-          else
-            error = 'Invalid access'
-            status = :not_acceptable
-          end
-        else
-          error = 'Authorization not found'
-          status = :not_found
-        end
-        if error
-          render json: { error: error }, status: status
-        end
-      end
-
-      get '/authorize' do
-        if (redirect_uri = params[:redirect_uri]) && redirect_uris.include?(redirect_uri)
-          auth = app.create_authorization!(
-            namespace: app.namespace,
-            scopes: 'openid profile email session_access multi_tenant create read update delete digest',
-            metadata: { redirect_uri: redirect_uri, state: params[:state] }
-          )
-          authorize(auth)
-        else
-          render json: { error: "Invalid redirect_uri param: #{redirect_uri}" }, status: :bad_request
-        end
-      end
-
       post '/config' do
         access_token = request.headers['Authorization'].to_s.split(' ')[1].to_s
         if (user_id = app.user_id_for(access_token))
@@ -88,25 +41,6 @@ module Cenit
           else
             render json: { error: 'Invalid payload' }, status: :bad_request
           end
-        else
-          render json: { error: 'Invalid code' }, status: :unauthorized
-        end
-      end
-
-      post '/token' do
-        auth = nil
-        value = request.body.read
-        if (code = Code.where(value: value).first) && code.active?
-          auth = Setup::Authorization.where(id: code.metadata['auth_id']).first
-          code.destroy
-        end
-        if auth
-          render json: {
-            access_token: auth.access_token,
-            expiration_date: (auth.authorized_at || Time.now) + (auth.token_span || 0),
-            id_token: auth.id_token
-          }
-          auth.destroy
         else
           render json: { error: 'Invalid code' }, status: :unauthorized
         end
