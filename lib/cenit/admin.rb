744f2261d3440d70a4d814a6b0293524de669a71
Maikel Arcia Carrazana <macarci@gmail.com>
2021-11-24 18:40:55 -0300

    Update | Default cloud URL

--- lib/cenit/admin.rb
+++ lib/cenit/admin.rb
@@ -9,7 +9,7 @@ module Cenit
 
     oauth_authorization_for 'openid profile email session_access multi_tenant create read update delete digest'
 
-    DEFAULT_CLOUD_URL = 'http://cenit-admin.s3-website-us-west-2.amazonaws.com'
+    DEFAULT_CLOUD_URL = 'https://cenit-ui.io'
 
     default_url DEFAULT_CLOUD_URL
   end
