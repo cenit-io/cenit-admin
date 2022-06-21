973f2265724a36033e34a0cad1666300b914e7ca
Miguel Sancho Fernandez <sanchocuba@gmail.com>
2022-06-20 21:42:18 -0400

    Update admin.rb

--- lib/cenit/admin.rb
+++ lib/cenit/admin.rb
@@ -9,7 +9,7 @@ module Cenit
 
     oauth_authorization_for 'openid profile email session_access multi_tenant create read update delete digest'
 
-    DEFAULT_CLOUD_URL = 'https://app.cenit.io'
+    DEFAULT_CLOUD_URL = 'http://localhost:3002'
 
     default_url DEFAULT_CLOUD_URL
   end
